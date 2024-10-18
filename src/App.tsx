3b83d14da845c9f8829ede69df608d696af27112
Jude Zambarakji <judezambarakji@gmail.com>
2024-10-18 12:31:38 +0300

    Separated search bar background from tile background.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Search from "./components/Search";
 import Forecast from "./components/Forecast";
 import useForecast from "./hooks/useForecast";
@@ -5,20 +6,29 @@ import useForecast from "./hooks/useForecast";
 const App = (): JSX.Element => {
   const [location, options, forecast, onInputChange, onOptionSelect, onSubmit] =
     useForecast();
+  const [showSearch, setShowSearch] = useState(true);
+
+  const handleSubmit = () => {
+    onSubmit();
+    setShowSearch(false);
+  };
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 min-h-screen w-full p-4">
-      <div className="w-full max-w-[500px] sm:max-w-[600px] md:max-w-[700px] lg:max-w-[800px] xl:max-w-[900px]">
-        {forecast ? (
-          <Forecast data={forecast} />
-        ) : (
+      <div className="w-full max-w-[500px] sm:max-w-[600px] md:max-w-[700px] lg:max-w-[800px] xl:max-w-[900px] flex flex-col items-center">
+        {showSearch && (
           <Search
             location={location}
             options={options}
             onInputChange={onInputChange}
             onOptionSelect={onOptionSelect}
-            onSubmit={onSubmit}
+            onSubmit={handleSubmit}
           />
+        )}
+        {forecast && (
+          <div className="w-full mt-4 bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg p-4 md:p-6">
+            <Forecast data={forecast} />
+          </div>
         )}
       </div>
     </main>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent } from "react";
-import { optionType } from "./types";
+import { optionType } from "../types";
 
 type Props = {
   location: string;
@@ -17,41 +17,42 @@ const Search = ({
   onSubmit,
 }: Props): JSX.Element => {
   return (
-    <section
-      className="w-full bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg
-      text-zinc-700 rounded p-4 md:p-6
-      flex flex-col justify-center
-      h-[200px] sm:h-[230px] md:h-[260px] lg:h-[300px]"
-    >
-      <div className="w-full">
-        <h1 className="text-4xl font-thin text-center mb-4">
+    <section className="w-full flex justify-center">
+      {" "}
+      {/* Added flex and justify-center */}
+      <div className="w-1/2 bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded p-4 md:p-6">
+        {" "}
+        {/* Added w-1/2 */}
+        <h1 className="text-4xl font-thin text-center mb-4 text-zinc-700">
           Weather <span className="font-black">Forecast</span>
         </h1>
-        <p className="text-sm text-center mb-4">
+        <p className="text-sm text-center mb-4 text-zinc-700">
           Enter location and select an option from the dropdown menu.
         </p>
-        <div className="relative flex mt-2">
-          <input
-            type="text"
-            value={location}
-            onChange={onInputChange}
-            className="px-2 py-1 rounded-l-md border-2 border-white 
-             hover:border-zinc-500 hover:text-zinc-500
-             text-zinc-800 focus:outline-none w-full"
-          />
-          <ul className="absolute top-9 bg-white ml-1 rounded-b-md w-full">
-            {options.map((option: optionType, index: number) => (
-              <li key={option.name + "-" + index}>
-                <button
-                  className="text-left text-sm w-full hover:bg-zinc-700
-                 hover:text-white px-2 py-1 cursor-pointer"
-                  onClick={() => onOptionSelect(option)}
-                >
-                  {option.name}, {option.country}
-                </button>
-              </li>
-            ))}
-          </ul>
+        <div className="relative flex mt-2 justify-center">
+          <div className="w-full">
+            <input
+              type="text"
+              value={location}
+              onChange={onInputChange}
+              className="px-2 py-1 rounded-l-md border-2 border-white 
+               hover:border-zinc-500 hover:text-zinc-500
+               text-zinc-800 focus:outline-none w-full"
+            />
+            <ul className="absolute top-9 bg-white ml-1 rounded-b-md w-full">
+              {options.map((option: optionType, index: number) => (
+                <li key={option.name + "-" + index}>
+                  <button
+                    className="text-left text-sm w-full hover:bg-zinc-700
+                   hover:text-white px-2 py-1 cursor-pointer"
+                    onClick={() => onOptionSelect(option)}
+                  >
+                    {option.name}, {option.country}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
           <button
             className="rounded-r-md border-2 border-zinc-100
           hover:border-zinc-500 hover:text-zinc-500 
